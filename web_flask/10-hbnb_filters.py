622c7f7f8df90ae800c08a560ebe3e4f090f81ef
Kehinde Bobade <kennygbeminiyi@gmail.com>
2022-10-17 00:39:30 +0100

    hbnb filters

--- web_flask/10-hbnb_filters.py
+++ web_flask/10-hbnb_filters.py
@@ -3,14 +3,16 @@
 from flask import Flask, render_template
 from models import storage
 from models.state import State
+from models.amenity import Amenity
 
 app = Flask(__name__)
 
 
-@app.route("/states", strict_slashes=False)
-@app.route("/states/<id>", strict_slashes=False)
-def states(id=None):
-    return render_template('9-states.html', states=storage.all(State), id=id)
+@app.route("/hbnb_filters", strict_slashes=False)
+def hbnb_filters():
+    states = storage.all(State)
+    amenities = storage.all(Amenity)
+    return render_template('10-hbnb_filters.html', states=states, amenities=amenities)
 
 
 @app.teardown_appcontext
