a84ba7cd3d321b86a53a0f4008a4e79bb966ded8
Kehinde Bobade <kennygbeminiyi@gmail.com>
2022-10-17 00:55:29 +0100

    advanced

---
+++ web_flask/100-hbnb.py
@@ -0,0 +1,26 @@
+#!/usr/bin/python3
+"""starts a Flask web application:"""
+from flask import Flask, render_template
+from models import storage
+from models.state import State
+from models.amenity import Amenity
+from models.place import Place
+
+app = Flask(__name__)
+
+
+@app.route("/hbnb", strict_slashes=False)
+def hbnb():
+    states = storage.all(State)
+    amenities = storage.all(Amenity)
+    places = storage.all(Place)
+    return render_template('10-hbnb_filters.html', states=states, amenities=amenities, places=places)
+
+
+@app.teardown_appcontext
+def teardown(arg=None):
+    storage.close()
+
+
+if __name__ == "__main__":
+    app.run(host="0.0.0.0", port="5000")
