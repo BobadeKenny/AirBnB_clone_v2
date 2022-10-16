e128c35a774fcc555c87963c6f69fbf6013b6d5e
Kehinde Bobade <kennygbeminiyi@gmail.com>
2022-10-16 22:38:55 +0100

    cities by state

---
+++ web_flask/9-states.py
@@ -0,0 +1,21 @@
+#!/usr/bin/python3
+"""starts a Flask web application:"""
+from flask import Flask, render_template
+from models import storage
+from models.state import State
+
+app = Flask(__name__)
+
+
+@app.route("/states", strict_slashes=False)
+def states_list():
+    return render_template('9-states.html', states=storage.all(State))
+
+
+@app.teardown_appcontext
+def teardown(arg=None):
+    storage.close()
+
+
+if __name__ == "__main__":
+    app.run(host="0.0.0.0", port="5000")
